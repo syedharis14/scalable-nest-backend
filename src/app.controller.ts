6f3df66c3045c511488ef70109fec551ea36b347
syedharis14 <sharisdev08@gmail.com>
2025-03-18 17:12:38 +0500

    feat(auth): secure app controller with JWT authentication

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, UseGuards } from "@nestjs/common";
+import { JwtAuthGuard } from "./auth/jwt-auth.guard";
 
-@Controller()
+@Controller("protected")
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    getProtectedData() {
+        return { message: "This is protected data" };
+    }
 }
