23e589da60e1e5debb776609d0b4dee6981c8065
syedharis14 <sharisdev08@gmail.com>
2025-03-18 17:12:46 +0500

    test(auth): improve AuthService spec with user registration test

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,21 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AuthService } from './auth.service';
+import { JwtService } from "@nestjs/jwt";
+import { Test, TestingModule } from "@nestjs/testing";
+import { PrismaService } from "../prisma/prisma.service";
+import { AuthService } from "./auth.service";
 
-describe('AuthService', () => {
-  let service: AuthService;
+describe("AuthService", () => {
+    let authService: AuthService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            providers: [AuthService, PrismaService, JwtService]
+        }).compile();
 
-    service = module.get<AuthService>(AuthService);
-  });
+        authService = module.get<AuthService>(AuthService);
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+    it("should register a user", async () => {
+        const user = await authService.register("test@example.com", "password");
+        expect(user.email).toBe("test@example.com");
+    });
 });
