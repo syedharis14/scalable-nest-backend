d29d7e984ed36666f7dc022b96b43b6eb829c448
syedharis14 <sharisdev08@gmail.com>
2025-03-20 02:30:18 +0500

    feat(auth): update register and login to return types

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
@@ -6,12 +7,18 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('register')
-  async register(@Body() body: { email: string; password: string }) {
-    return this.authService.register(body.email, body.password);
+  async register(
+    @Body() body: { email: string; password: string },
+  ): Promise<User> {
+    const user = await this.authService.register(body.email, body.password);
+    return user;
   }
 
   @Post('login')
-  async login(@Body() body: { email: string; password: string }) {
-    return this.authService.login(body.email, body.password);
+  async login(
+    @Body() body: { email: string; password: string },
+  ): Promise<{ token: string }> {
+    const result = await this.authService.login(body.email, body.password);
+    return result;
   }
 }
